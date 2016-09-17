2ee103bd164fd915f22164cf00fb40126e6b5b59
Serge-Olivier Amega <sergeolivieramega@gmail.com>
2016-09-17 12:44:03 -0400

    init

--- app.js
+++ app.js
@@ -1,43 +1,26 @@
-const http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
+const express = require('express'),
+      path    = require('path'),
+      sysInfo = require('./utils/sys-info.js'),
+      env     = process.env;
 
-let server = http.createServer(function (req, res) {
-  let url = req.url;
-  if (url == '/') {
-    url += 'index.html';
-  }
+let app = express();
 
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
+app.get("/a|b", (req, res) => {
+    res.send("hi");
+});
 
-  if (url == '/health') {
+app.get("health", (req, res) => {
     res.writeHead(200);
     res.end();
-  } else if (url == '/info/gen' || url == '/info/poll') {
+});
+
+app.get("/info/(gen|poll)", (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Cache-Control', 'no-cache, no-store');
     res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-  } else {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end('Not found');
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
-      }
-    });
-  }
 });
 
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', () => {
   console.log(`Application worker ${process.pid} started...`);
 });
+
